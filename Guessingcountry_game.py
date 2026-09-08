ebab93720788953fe6baa3d9397feaa7091714fd
412-22-natcharee <54671@yupparaj.ac.th>
2026-09-08 14:57:34 +0700

    Update Guessingcountry_game.py

--- Guessingcountry_game.py
+++ Guessingcountry_game.py
@@ -211,3 +211,18 @@ st.session_state.ans9_val = ans9
 st.session_state.ans10_val = ans10
 
 # 4. ปุ่มส่งคำตอบ
+if "start" in st.session_state and not st.session_state.get("is_ended", False):
+   if st.button("📥 ส่งคำตอบ"):
+       st.session_state.is_ended = True
+       st.rerun()
+
+   time.sleep(1)
+   st.rerun()
+
+# 5. แสดง Dialog ผลลัพธ์
+if st.session_state.get("is_ended", False):
+    show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans10)
+
+
+st.divider()
+st.write("นางสาวณัฐชารี คงสุนทร เลขที่ 22 ม.4/12")
